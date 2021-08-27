9377b577428a3003aea951a4cf031389cc35baed
AHJ <AHJ@ahj.kr>
2021-08-28 00:25:02 +0900

    nomadjs 3.8.2 toggle

--- jsdoing.js
+++ jsdoing.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleh1Click() {
-    const clickedClass = 'active';
-   if(h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);//classList를 사용함으로써 html에 원래 붙어있던 class='original'은 사라지지 않게 되었다.
-    }
+    h1.classList.toggle('active');
 }
-
+//toggle function은 앞선 과정들이 이미 많이 행해지고 있기때문에 기존에 이미 같은 방식으로 동작되도록 해주는 함수로 만들어져 있는것.. 이렇게 간단하게 1줄로 구현할 수 있다!
 h1.addEventListener('click', handleh1Click);
