9317cb718b90bd0595db5c88cb4a7d9f4e659c78
AHJ <AHJ@ahj.kr>
2021-08-27 14:42:42 +0900

    namadjs 3.7.1 using class

--- jsdoing.js
+++ jsdoing.js
@@ -1,14 +1,11 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleh1Click() {
-    const currntColor = h1.style.color;
-    let newColor;
-    if (currntColor==='blue') {
-        newColor = 'tomato';
-    } else {
-        newColor = 'blue';
+   if(h1.className === 'active') {//css의 .active class이름과 같아야 한다
+    h1.className = '';
+    } else{
+        h1.className = 'active';//문제는 여기도 active 2번 쓰고 있음
     }
-    h1.style.color = newColor;//중복이 좀 줄긴 했다 하지만 여전히 style을 js에서 편집하는거는 바람직하지 못하다 css로 가져가서 할 수 있도록 해보자
 }
 
 h1.addEventListener('click', handleh1Click);
