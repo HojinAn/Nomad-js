54efd5d0569b46755a24a490ed5076b8cabe0232
AHJ <AHJ@ahj.kr>
2021-08-27 13:30:35 +0900

    nomadjs 3.6.1 duplication

--- jsdoing.js
+++ jsdoing.js
@@ -1,40 +1,11 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleh1Click() {
-    h1.style.color='blue';
-}
-
-function handleMouseEnter() {
-    h1.innerText = 'Mouse is here!';
-}
-
-function handleMouseLeave() {
-    h1.innerText = 'Mouse is gone!';
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = 'tomato';
-}
-
-function handleWindowCopy() {
-    alert('copier!');
-}
-
-function handleWindowOffline() {
-    alert('SOS no WIFI');
-}
-
-function handleWindowOnline() {
-    alert('ALL GOOD');
+    if (h1.style.color==='blue') {//js는 if문 돌릴때 ===로 같음을 표시 파이썬이랑 다르네
+        h1.style.color = 'tomato';
+    } else {
+        h1.style.color = 'blue';//이렇게 하면 'blue'가 중복이라 별로임
+    }
 }
 
 h1.addEventListener('click', handleh1Click);
-h1.addEventListener('mouseenter', handleMouseEnter);
-h1.addEventListener('mouseleave', handleMouseLeave);// onmoustleave보다 이걸 더 선호하는 이유? .removeEventListener로 event listener르르 지울 수 있음
-
-//document처럼 window도 기본적으로 제공하는 object다
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
-
