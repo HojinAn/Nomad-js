ba86d305e60186a2eb5d3706c6be51c8ca4f0d80
AHJ <AHJ@ahj.kr>
2021-08-27 23:59:00 +0900

    nomadjs 3.7.2 html class altered problem

--- jsdoing.js
+++ jsdoing.js
@@ -1,10 +1,11 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleh1Click() {
-   if(h1.className === 'active') {//css의 .active class이름과 같아야 한다
+    const clickedClass = 'active';//raw value는 오류나기 쉬움
+   if(h1.className === clickedClass) {//이걸 오타내면 변수는 js가 문제를 알려준다
     h1.className = '';
     } else{
-        h1.className = 'active';//문제는 여기도 active 2번 쓰고 있음
+        h1.className = clickedClass;//html의 class가 얘땜에 대체되어버린다 문제 -> html의 class는 변경하지 않을 방법을 찾아야한다
     }
 }
 
