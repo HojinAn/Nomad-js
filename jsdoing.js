9bb13c0f53582c2a560dced4b6cdaac93064f8ec
AHJ <asdv94@naver.com>
2021-08-30 08:37:11 +0900

    nomad 4.0.1 same as practice1

--- jsdoing.js
+++ jsdoing.js
@@ -1,3 +1,2 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
