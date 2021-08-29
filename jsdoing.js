df07001ed6d618005fe5556e152d312e7e955a4a
AHJ <asdv94@naver.com>
2021-08-30 08:27:23 +0900

    4.0.1 html element in js practice1

--- jsdoing.js
+++ jsdoing.js
@@ -1,7 +1,3 @@
-const h1 = document.querySelector('div.hello:first-child h1');
-
-function handleh1Click() {
-    h1.classList.toggle('active');
-}
-//toggle function은 앞선 과정들이 이미 많이 행해지고 있기때문에 기존에 이미 같은 방식으로 동작되도록 해주는 함수로 만들어져 있는것.. 이렇게 간단하게 1줄로 구현할 수 있다!
-h1.addEventListener('click', handleh1Click);
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
