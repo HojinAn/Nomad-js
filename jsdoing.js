a4a8b332bfb7532c0a32d93cd3ea4feeef610362
AHJ <asdv94@naver.com>
2021-08-30 08:44:40 +0900

    4.0.2 eventlistener get value

--- jsdoing.js
+++ jsdoing.js
@@ -1,2 +1,8 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+
+function onLoginbtnClick() {
+  console.log("hello", loginInput.value);
+}
+
+loginButton.addEventListener("click", onLoginbtnClick);
