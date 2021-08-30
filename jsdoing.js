92e26d7cb23d63fd77f9c17c6de6c01c228e7e56
AHJ <asdv94@naver.com>
2021-08-30 17:10:40 +0900

    4.2.1 js submit, function argument=information

--- jsdoing.js
+++ jsdoing.js
@@ -1,9 +1,10 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginbtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(information) {
+  information.preventDefault();
+  console.dir(information);
+  console.log(information); //이렇게 function으로 주어진 infrormation 확인 가능
 }
 
-loginButton.addEventListener("click", onLoginbtnClick);
+loginForm.addEventListener("submit", onLoginSubmit); //click event 대신 submit event로 대체
