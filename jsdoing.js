ccb9c39308e3ea59034af1a8f17892045875e36f
AHJ <asdv94@naver.com>
2021-08-30 17:49:33 +0900

    4.4.1 add hidden class on CSS and duplication happens

--- jsdoing.js
+++ jsdoing.js
@@ -1,18 +1,13 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
 function onLoginSubmit(information) {
   information.preventDefault();
-  console.dir(information);
-  console.log(loginInput.value); //이렇게 function으로 주어진 infrormation 확인 가능
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove("hidden"); //'hidden을 2번 이상 썼으니 제거하고 통일하고 싶다!
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); //click event 대신 submit event로 대체
-link.addEventListener("click", handleLinkClick);
