44df40f746089adc2ebf77cf2bb02b50d21371c5
AHJ <asdv94@naver.com>
2021-08-30 17:34:11 +0900

    4.3 preventDefault practice

--- jsdoing.js
+++ jsdoing.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(information) {
   information.preventDefault();
   console.dir(information);
-  console.log(information); //이렇게 function으로 주어진 infrormation 확인 가능
+  console.log(loginInput.value); //이렇게 function으로 주어진 infrormation 확인 가능
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); //click event 대신 submit event로 대체
+link.addEventListener("click", handleLinkClick);
