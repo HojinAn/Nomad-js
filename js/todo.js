0ab53cd42961882cb9e30e55e9b550bb8920cd64
AHJ <asdv94@naver.com>
2021-09-05 00:18:45 +0900

    7.2.2 check which button was clicked by consolelog event argument

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,9 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function deleteToDo() {
-  console.log("don't know which one is clicked")//여러개의 button이 생겨도 무엇이 클릭됐는지 모른다 아직
+function deleteToDo(event) {
+  //click event정보가 온다
+  console.log(event); //어떤 button이 클릭되었나 단서 얻기 위해 consolo log하고서 path를 봐준다.
 }
 
 function paintToDo(newToDo) {
@@ -11,11 +12,11 @@ function paintToDo(newToDo) {
   const span = document.createElement("span");
   span.innerText = newToDo;
   const button = document.createElement("button");
-  button.innerText = "❌"; //윈도우 기준 이모지 단축키는 '윈도우 + .' 입니다
+  button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  li.appendChild(button); //먼저 span을 추가하고 button을 추가했다 button이 span다음에 위치하게 될 것
-  toDoList.appendChild(li); //span, button을 해주고나서야 li를 toDoList에 추가
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
