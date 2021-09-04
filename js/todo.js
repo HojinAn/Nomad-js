c41b794021d18da34063af28ac930be4c4138b08
AHJ <asdv94@naver.com>
2021-09-05 00:50:59 +0900

    7.3.1 save data on localstorage but refresh page clears it

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = []; //input한 data들을 loalStorage에 저장하고 싶은데 우선 만들어준 것이 array!
+
+function saveToDos() {
+  localStorage.setItem("todos", toDos); //입력된 아래 toDos array를 localStorage에 넣어준다.
+}
+
 function deleteToDo(event) {
-  const li = event.target.parentElement; //클릭된 target은 button이고 button의 parentelement는 li다.
-  li.remove(); //button이 눌렸지만 부모인 li를 지움으로써 한줄 삭제!
+  const li = event.target.parentElement;
+  li.remove();
 }
 
 function paintToDo(newToDo) {
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo); //여기 써먹으려고 위에서 array 선언 이걸 local storage에 넣어줄거다 하지만 localstorage는 text만 저장가능하고 array는 불가하다.
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
