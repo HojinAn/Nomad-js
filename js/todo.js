9d8ccd12df2fa96edb416e2e622936ead497a4fc
AHJ <asdv94@naver.com>
2021-09-05 01:06:39 +0900

    7.3.2 JSONstringify changes JS to string

--- js/todo.js
+++ js/todo.js
@@ -5,7 +5,12 @@ const toDoList = document.getElementById("todo-list");
 const toDos = []; //input한 data들을 loalStorage에 저장하고 싶은데 우선 만들어준 것이 array!
 
 function saveToDos() {
-  localStorage.setItem("todos", toDos); //입력된 아래 toDos array를 localStorage에 넣어준다.
+  // localStorage.setItem("todos", toDos); 하지만 HTML은 array를 저장할 수 없기 때문에 이렇게 해주면 각각의 값들이 array가 아닌(["a","b","c"]) 그냥 text로 저장된다.(a,b,c)
+  localStorage.setItem(
+    "todos",
+    JSON.stringify(toDos)
+  ); /*array의 텍스트 모양 자체를 string으로 저장해버리면 된다! 그걸 해주는 역할이 JSON.stringify 얘가 toDos array의 ["a", "b", "c"] 뭐 이런 형태를 localStorage에 그대로 저장할 수 있도록 해주는 것이다. 
+ 즉, JSON.stringify는 어떤 JavaScript 코드던 간에 그걸 string으로 바꿔준다.*/
 }
 
 function deleteToDo(event) {
