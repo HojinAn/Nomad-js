fa682d303da13a1ac8be60e3830830c15706bacd
AHJ <asdv94@naver.com>
2021-09-05 16:20:37 +0900

    7.4.2 foreach for each item in array using arrow function just same as 7.4.1

--- js/todo.js
+++ js/todo.js
@@ -38,12 +38,13 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+/*
 function sayHello(item) {
   console.log("this is the turn of", item);
-}
+} 이렇게 따로 function 만드는 대신에 */
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
-  const parsedToDos = JSON.parse(savedToDos); //이거 통해서 string으로 쓰여있던 걸 array로 변환해준다. array 각각의 item에 대해 function을 실행할 수 있다. 화면에 뿌려주던가 텍스트를 변형하거나 JS는 이것을 가능하게 해준다.
-  parsedToDos.forEach(sayHello); //foreach는 array 각각의 item에 대해 function을 실행할수 있게 해준다.
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn of ", item)); //이렇게 간단하게 한줄로 함수 따로 정의하지 않고 써줄 수 있다. 완전 똑같다. parseToDos 각각의 item에 대해 실행하게 된다. arrowfunction이라고 한다.
 }
