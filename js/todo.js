0fdc3c03314735268b90815a3fd37d8ab6b289c3
AHJ <asdv94@naver.com>
2021-09-05 16:29:52 +0900

    7.5.1 show loclaStorage data but clearing prob

--- js/todo.js
+++ js/todo.js
@@ -38,13 +38,9 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-/*
-function sayHello(item) {
-  console.log("this is the turn of", item);
-} 이렇게 따로 function 만드는 대신에 */
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item)); //이렇게 간단하게 한줄로 함수 따로 정의하지 않고 써줄 수 있다. 완전 똑같다. parseToDos 각각의 item에 대해 실행하게 된다. arrowfunction이라고 한다.
+  parsedToDos.forEach(paintToDo); //이렇게 해주면 localStorage에 저장된 값들을 출력해 줄 수 있다. 하지만 저기 위에서 애초에 toDos를 빈 array로 정의해놔서 새로고침한 후에 값을 넣어주면 초기화돼버리는 문제가 있다.
 }
